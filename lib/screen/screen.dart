2f7407ed2fca2e081247b19be51c9ba773334a6f
m-augustyniak <m.augustyniak@fivedottwelve.com>
2022-11-30 18:46:45 +0100

    UI part

--- lib/screen/screen.dart
+++ lib/screen/screen.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:scanera/ext/context_ext.dart';
+import 'package:scanera/theme/color/app_colors.dart';
 import 'package:scanera/theme/text/app_typography.dart';
+import 'package:scanera/widget/config_dropdown.dart';
+import 'package:scanera/widget/dialog/configuration_dialog.dart';
+import 'package:scanera/widget/dialog/info_dialog.dart';
+import 'package:scanera/widget/options_bottom_sheet.dart';
+import 'package:scanera/widget/page_app_bar.dart';
+import 'package:scanera/widget/scan_controller.dart';
+import 'package:scanera/widget/tile/sensor_tile.dart';
+import 'package:scanera/widget/tile/signal_tile.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -17,19 +28,172 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          widget.title,
-          style: AppTypography().white.headlineLarge,
-        ),
+      resizeToAvoidBottomInset: false,
+      appBar: PageAppBar(
+        title: "Scanera",
+        leftIcon: Icons.arrow_back,
+        rightIcon: Icons.more_vert,
+        onRightIconTap: () => openOptions(),
+        onLeftTap: () => GoRouter.of(context).pop(),
       ),
-      body: Center(
-        child: Text(
-          'Scanera',
-          style: AppTypography().black.headlineLarge,
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        unselectedItemColor: AppColors.kPrimary90,
+        unselectedFontSize: 11,
+        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        selectedFontSize: 11,
+        selectedItemColor: AppColors.kPrimaryBlue,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.bluetooth),
+            label: AppLocalizations.of(context).navBarBluetooth,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.wifi),
+            label: AppLocalizations.of(context).navBarWifi,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.edgesensor_high),
+            label: AppLocalizations.of(context).navBarSensors,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.perm_scan_wifi),
+            label: AppLocalizations.of(context).navBarAllScan,
+          ),
+        ],
+        onTap: (index) => _showConfigDialog(),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                ScanController(
+                  onPressedFirst: () => _showSettingsDialog(),
+                  onPressedSecond: () => displaySnackBar(
+                      AppLocalizations.of(context).homeSnackBarInfo),
+                  time: const Duration(
+                    minutes: 1,
+                    seconds: 32,
+                  ),
+                  coordinates: const [5, 10],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SignalTile(
+                  SSID: "Beacon (1)",
+                  BSSID: "0a766574-ee7a-4d43-b717-7f51ccad3ff6",
+                  level: -50,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    SensorTile(
+                      label: "Magnetometer",
+                      data: [-50, -50, -50],
+                    ),
+                    SensorTile(
+                      label: "Accelerometer",
+                      data: [-50, -50, -50],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ConfigDropdown(
+                  data: ["Config 1", "Config 2", "Config 3"],
+                )
+              ],
+            ),
+          ),
+          const Spacer(),
+          const Divider(
+            thickness: 1,
+            color: AppColors.kSurfaceVariant,
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> openOptions() async {
+    await showModalBottomSheet<void>(
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(25),
+          topLeft: Radius.circular(25),
+        ),
+      ),
+      context: context,
+      builder: (BuildContext context) => OptionsBottomSheet(
+        onOptionTap: (int index) => print(index),
+        options: [
+          AppLocalizations.of(context).homeBottomSheetOptionFirst,
+          AppLocalizations.of(context).homeBottomSheetOptionSecond,
+          AppLocalizations.of(context).homeBottomSheetOptionThird,
+        ],
+      ),
+    );
+  }
+
+  void displaySnackBar(String message) {
+    final snackBar = SnackBar(
+      backgroundColor: AppColors.kPrimary95,
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      duration: const Duration(
+        seconds: 10,
+      ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+      ),
+      content: Text(
+        message,
+        style: AppTypography().black.bodyLarge,
+      ),
+    );
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<bool?> _showSettingsDialog() async {
+    final result = await showDialog<bool>(
+      context: context,
+      builder: (_) => InfoDialog(
+        label: AppLocalizations.of(context).settingsDialogLabel,
+        content: AppLocalizations.of(context).settingsDialogContent,
+        rightOptionTitle:
+            AppLocalizations.of(context).settingsDialogOptionSecond,
+        leftOptionTitle: AppLocalizations.of(context).settingsDialogOptionFirst,
+      ),
+    );
+    return result;
+  }
+
+  Future<void> _showConfigDialog() async {
+    final result = await showDialog<void>(
+      context: context,
+      builder: (_) => const ConfigDialog(),
+    );
+    return result;
   }
 }
---
+++ lib/widget/page_app_bar.dart
@@ -0,0 +1,99 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/material.dart';
+import 'package:scanera/theme/color/app_colors.dart';
+import 'package:scanera/theme/text/app_typography.dart';
+
+class PageAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const PageAppBar({
+    super.key,
+    required this.title,
+    required this.leftIcon,
+    this.rightIcon,
+    required this.onLeftTap,
+    this.onRightIconTap,
+    this.isFileView = false,
+  });
+
+  final String title;
+  final IconData? rightIcon;
+  final IconData? leftIcon;
+  final Function()? onRightIconTap;
+  final Function()? onLeftTap;
+  final bool isFileView;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipSmoothRect(
+      radius: const SmoothBorderRadius.only(
+        bottomLeft: SmoothRadius(
+          cornerRadius: 20,
+          cornerSmoothing: 1,
+        ),
+        bottomRight: SmoothRadius(
+          cornerRadius: 20,
+          cornerSmoothing: 1,
+        ),
+      ),
+      child: Container(
+        color: AppColors.kPrimaryBlue,
+        child: SafeArea(
+          child: Container(
+            height: 74,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+            ),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: onLeftTap,
+                  child: SizedBox(
+                    width: 80,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Icon(
+                        leftIcon,
+                        size: 30,
+                        color: AppColors.kWhite,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: AutoSizeText(
+                      title,
+                      style: AppTypography().white.titleLarge,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      maxFontSize: 28,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 80,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: rightIcon != null
+                        ? InkWell(
+                            onTap: onRightIconTap,
+                            child: Icon(
+                              rightIcon,
+                              size: 30,
+                              color: AppColors.kWhite,
+                            ),
+                          )
+                        : const SizedBox(),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+}
